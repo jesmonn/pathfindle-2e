89dcd44ec9e3361df24c809eed8dd556f4cef954
jesmonn <argyadiputra@gmail.com>
2024-07-16 14:06:16 +0700

    Fixing links

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
         backgroundColor: 'darkred',
         boxShadow: '0px 0px 15px #8B0000',
       }}>
-        <Link to={"/"} style={{...titleTextStyleBold, fontSize: 25, marginLeft: '20vw', marginRight: '2vw'}}>Daily</Link>
-        <Link to={"/unlimited"} style={{...titleTextStyleBold, fontSize: 25, marginLeft: '2vw', marginRight: '20vw'}}>Unlimited</Link>
+        <Link to={"/pathfindle-2e/"} style={{...titleTextStyleBold, fontSize: 25, marginLeft: '20vw', marginRight: '2vw'}}>Daily</Link>
+        <Link to={"/pathfindle-2e/unlimited"} style={{...titleTextStyleBold, fontSize: 25, marginLeft: '2vw', marginRight: '20vw'}}>Unlimited</Link>
       </div>
       <Routes>
-        <Route path="/" element={<Daily />} />
-        <Route path="/unlimited" element={<Unlimited />} />
+        <Route path="/pathfindle-2e/" element={<Daily />} />
+        <Route path="/pathfindle-2e/unlimited" element={<Unlimited />} />
         <Route path="*" element={<h1 style={{...titleTextStyleBold, color: 'black', alignSelf: 'center'}}> That's Odd. There's nothing here. </h1>} />
       </Routes>
     </Router>
